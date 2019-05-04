78dc483b1c2bd2c6242061ecbf411b729a133dcb
j24576931000 <31430607+j24576931000@users.noreply.github.com>
2019-05-04 18:55:16 +0800

    Update install.js

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -44,8 +44,15 @@ function saveBeforeInstallPromptEvent(evt) {
 function installPWA(evt) {
   // CODELAB: Add code show install prompt & hide the install button.
 
-  // CODELAB: Log user response to prompt.
-
+  deferredInstallPrompt.userChoice
+    .then((choice) => {
+      if (choice.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt', choice);
+      } else {
+        console.log('User dismissed the A2HS prompt', choice);
+      }
+      deferredInstallPrompt = null;
+    });
 }
 
 // CODELAB: Add event listener for appinstalled event
