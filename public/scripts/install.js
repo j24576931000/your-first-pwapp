70c5e39fa78905de8ff8d1dabb4ccf825af13b25
j24576931000 <31430607+j24576931000@users.noreply.github.com>
2019-05-04 18:53:01 +0800

    Update install.js

--- public/scripts/install.js
+++ public/scripts/install.js
@@ -21,7 +21,7 @@ let deferredInstallPrompt = null;
 const installButton = document.getElementById('butInstall');
 installButton.addEventListener('click', installPWA);
 
-// CODELAB: Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
 
 /**
