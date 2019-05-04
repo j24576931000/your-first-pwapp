3e03fa3aac1195922b2f30ca77b9b7ee11bbe3b5
j24576931000 <31430607+j24576931000@users.noreply.github.com>
2019-05-04 18:45:23 +0800

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -162,7 +162,21 @@ function getForecastFromNetwork(coords) {
  * @return {Object} The weather forecast, if the request fails, return null.
  */
 function getForecastFromCache(coords) {
-  // CODELAB: Add code to get weather forecast from the caches object.
+ if (!('caches' in window)) {
+  return null;
+}
+const url = `${window.location.origin}/forecast/${coords}`;
+return caches.match(url)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
 
 }
 
